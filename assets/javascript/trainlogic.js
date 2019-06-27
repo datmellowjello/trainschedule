4d13e6a079e977292a7555274f46e174adfff2c1
davidbridges123 <david.bridges@live.com>
2019-06-26 22:26:24 -0600

    adding js

---
+++ assets/javascript/trainlogic.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyBQNUR2T2U1opMVZEvGeYYVMUaoACh2TyU",
+        authDomain: "trainproject-1b5e2.firebaseapp.com",
+        databaseURL: "https://trainproject-1b5e2.firebaseio.com",
+        projectId: "trainproject-1b5e2",
+        storageBucket: "",
+        messagingSenderId: "97815541965",
+        appId: "1:97815541965:web:3fadfeb7f618efd7"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+     
+      
+
+
+
+});
