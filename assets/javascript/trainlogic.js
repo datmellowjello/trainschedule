272db6743b97f1772ad477c99b032e9aa39e08bb
davidbridges123 <david.bridges@live.com>
2019-06-26 23:08:54 -0600

    updates

--- assets/javascript/trainlogic.js
+++ assets/javascript/trainlogic.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+console.log("test")
+$(document).ready(function () {
+    console.log("ready!");
 
     var firebaseConfig = {
         apiKey: "AIzaSyBQNUR2T2U1opMVZEvGeYYVMUaoACh2TyU",
@@ -9,13 +10,13 @@ $(document).ready(function() {
         storageBucket: "",
         messagingSenderId: "97815541965",
         appId: "1:97815541965:web:3fadfeb7f618efd7"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-     
-      var trainData = firebase.database();
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-      $("#add-train").on("click", function(event) {
+    var trainData = firebase.database();
+
+    $("#add-train").on("click", function (event) {
         event.preventDefault();
 
         var trainName = $("#train-name-input").val().trim();
@@ -29,19 +30,58 @@ $(document).ready(function() {
         var newTrain = {
             name: trainName,
             destination: destinationInput,
-            firstTrain: firstTrain,
+            firstTrain: firstTime,
             frequency: frequencyInput,
         };
 
         trainData.ref().push(newTrain);
 
-        $("#freq-input").empty();
-        $("#train-name-input").empty();
-        $("#dest-name-input").empty();
-        $("#first-train-input").empty();
+        $("#freq-input").val("");
+        $("#train-name-input").val("");
+        $("#dest-name-input").val("");
+        $("#first-train-input").val("");
 
 
-      })
+    });
+    trainData.ref().on("child_added", function (childSnapshot) {
+        console.log(childSnapshot.val());
 
+        var tName = childSnapshot.val().name;
+        var tDestination = childSnapshot.val().destination;
+        var tFrequency = childSnapshot.val().frequency;
+        var tFirstTrain = childSnapshot.val().firstTrain;
+
+        var timeArr = tFirstTrain.split(":");
+        var trainTime = moment()
+            .hours(timeArr[0])
+            .minutes(timeArr[1]);
+        var maxMoment = moment.max(moment(), trainTime);
+        var tMinutes;
+        var tArrival;
+
+        if (maxMoment === trainTime) {
+            tArrival = trainTime.format("hh:mm A");
+            tMinutes = trainTime.diff(moment(), "minutes");
+        } else {
+
+            var differenceTimes = moment().diff(trainTime, "minutes");
+            var tRemainder = differenceTimes % tFrequency;
+            tMinutes = tFrequency - tRemainder;
+
+            tArrival = moment()
+                .add(tMinutes, "m")
+                .format("hh:mm A");
+        }
+
+        $("#train-table > tbody").append(
+            $("<tr>").append(
+                $("<td>").text(tName),
+                $("<td>").text(tDestination),
+                $("<td>").text(tFrequency),
+                $("<td>").text(tArrival),
+                $("<td>").text(tMinutes)
+            )
+        );
+    });
 
 });
